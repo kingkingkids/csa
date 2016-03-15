8c239a5fae46f6698fb43f603f985b8663655823
kingkingkids <kingkingkids@qq.com>
2016-03-15 14:24:51 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = {
             {
                 test: /\.html$/,
                 loader: "html-loader"
+            }, ,
+            {
+                test: /\.js/,
+                loader: "babel-loader"
             }
 
         ]
